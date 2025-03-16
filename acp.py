e3a3450434189a4439b84a1f353b81e1e56c72da
pookiedog11 <149290576+psychorax1433@users.noreply.github.com>
2025-03-16 13:41:42 +0530

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,7 @@
+# Get user input
+age = int(input("Enter your age: "))
+# Check if age is between 10 and 20
+if 10 <= age <= 20:
+    print("Your age is between 10 and 20 years.")
+else:
+    print ("Your age is not between 10 and 20 years.")
